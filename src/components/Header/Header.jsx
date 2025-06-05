3bbfaa4ae68360a3a482e351670a10499f33cc9a
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-05 13:13:04 -0300

    feat: implementa header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import './Header.css';
+import logo from '../../../public/logo.png';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DesktopNav from '../Navigation/DesktopNav';
+import MobileNav from '../Navigation/MobileNav';
+
+const Header = () => {
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 700);
+		};
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	useEffect;
+
+	return (
+		<header>
+			<div className='header__top-bar'></div>
+			<div className='header__container'>
+				<img
+					className='header__logo'
+					src={logo}
+					alt='Logotipo da Agência Rosa Chá'
+					onClick={() => navigate('/')}
+				/>
+				{!isMobile && <DesktopNav />}
+				{isMobile && <MobileNav />}
+			</div>
+		</header>
+	);
+};
+
+export default Header;
