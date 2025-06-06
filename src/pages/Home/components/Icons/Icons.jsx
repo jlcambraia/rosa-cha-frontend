941542dcf8374114769db5b23d1ae259b870231d
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-06 14:47:28 -0300

    feat: implementa seção Icons

---
+++ src/pages/Home/components/Icons/Icons.jsx
@@ -0,0 +1,18 @@
+import './Icons.css';
+import mockIcons from '../../../../data/mocks/icons.js';
+
+const Icons = () => {
+	return (
+		<section className='icons'>
+			<ul className='icons__list'>
+				{mockIcons.map((icon, index) => (
+					<li className='icon__container' key={index}>
+						<img className='icon__image' src={icon.url} alt={icon.alt} />
+					</li>
+				))}
+			</ul>
+		</section>
+	);
+};
+
+export default Icons;
