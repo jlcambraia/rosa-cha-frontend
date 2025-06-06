31f4f665465a0ec001b4b344ef05bfc1b8b71289
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-06 14:51:30 -0300

    feat: implementa App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
-function App() {
-	return <></>;
-}
+const App = () => {
+	return (
+		<>
+			<Header />
+			<Home />
+			<Footer />
+		</>
+	);
+};
 
 export default App;
