df856db20cf471a8977d8677659563c7d295e03e
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-06-10 10:30:39 -0300

    refactor: altera cor de fundo da seção

--- src/pages/Home/components/Icons/Icons.jsx
+++ src/pages/Home/components/Icons/Icons.jsx
@@ -4,13 +4,15 @@ import mockIcons from '../../../../data/mocks/icons.js';
 const Icons = () => {
 	return (
 		<section className='icons'>
-			<ul className='icons__list'>
-				{mockIcons.map((icon, index) => (
-					<li className='icon__container' key={index}>
-						<img className='icon__image' src={icon.url} alt={icon.alt} />
-					</li>
-				))}
-			</ul>
+			<div className='icons__container'>
+				<ul className='icons__list'>
+					{mockIcons.map((icon, index) => (
+						<li className='icon__container' key={index}>
+							<img className='icon__image' src={icon.url} alt={icon.alt} />
+						</li>
+					))}
+				</ul>
+			</div>
 		</section>
 	);
 };
